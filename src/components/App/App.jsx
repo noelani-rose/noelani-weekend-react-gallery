56b8659de19f666d54df4a9e8600d4b7c06d4a69
Noelani West <noelani.rose14@gmail.com>
2022-10-23 11:50:06 -0500

    create galleryitem component with return of picture and love button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import GalleryItem from '../GalleryItem/GalleryItem';
 
 function App() {
     return (
@@ -7,8 +8,7 @@ function App() {
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryItem />
       </div>
     );
 }
